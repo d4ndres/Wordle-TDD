518af7b045e61ccca02d74f83fe0522ea38a7a10
diego rojas <d4ndres@gmail.com>
2024-03-26 13:55:19 -0500

    refactor proyect

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import {VICTORY_MESSAGE, DEFEAT_MESSAGE} from '@/const/const'
+import {ref} from 'vue'
 defineProps<{ 
   wordOfTheDay: string
 }>()
+
+const guessInProgress = ref("")
+const guessSubmitted = ref("")
+
 </script>
 
 <template>
-  <input type="text">
-  <p>You won!</p>
-  <p>{{ wordOfTheDay }}</p>
+  <input type="text" v-model="guessInProgress" @keydown.enter="guessSubmitted = guessInProgress">
+  <p v-if="guessSubmitted.length" v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE" ></p>
 </template>
--- src/components/__tests__/wordleBoard.spec.ts
+++ src/components/__tests__/wordleBoard.spec.ts
@@ -2,18 +2,42 @@
 
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
+import {VICTORY_MESSAGE, DEFEAT_MESSAGE} from '@/const/const'
+
+
+
+
 
 describe('WordleBoard', () => {
-  test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
-    // Arrange
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TESTS'}})
-  
-    // Act
+  const wordOfTheDay = 'TESTS'
+  let wrapper : ReturnType<typeof mount>
+  beforeEach( () => {
+    wrapper = mount(WordleBoard, { props: { wordOfTheDay }})
+
+  })
+
+  async function playerSubmitsGuess(guess: string ){
     const guessInput = wrapper.find("input[type=text]")
-    await guessInput.setValue('TESTS')
+    await guessInput.setValue(guess)
     await guessInput.trigger("keydown.enter")
   
-    expect(wrapper.text()).toContain("You won!")
+  }
+
+
+  test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
+    await playerSubmitsGuess(wordOfTheDay)
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+  })
+
+
+  test("a defeat message appears if the user makes a guess that is incorrect", async ()=> {
+    await playerSubmitsGuess("WRONG")
+    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+  })
+
+  test("no end of game message appears if the user has not yet made a guess", async () => {
+    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
   })
 
 })
